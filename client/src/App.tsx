440875b3392fd78004ac9686077261cca1fe45ba
Nataliia Miroshnyk <natamrshn@gmail.com>
2025-02-02 20:31:31 +0000

    add react-router-dom and redux to project

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,19 @@
-import './App.css'
-import Home from './pages/Home'
+import { Provider } from "react-redux";
+import { Outlet } from "react-router-dom";
+import { store } from "./store";
+import Header from "./components/Header/Header";
 
-function App() {
-
+const App = () => {
   return (
-    <Home />
-  )
-}
+    <Provider store={store}>
+      <div>
+        <Header />
+        <main>
+          <Outlet />
+        </main>
+      </div>
+    </Provider>
+  );
+};
 
-export default App
+export default App;
